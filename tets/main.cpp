33d65943185c3eb28eafc56b004400e685587a0b
eddiieee <kontakt.lesiak@gmail.com>
2022-11-14 21:51:30 +0100

    to tylko test

--- tets/main.cpp
+++ tets/main.cpp
@@ -1,14 +1,7 @@
-//
-//  main.cpp
-//  tets
-//
-//  Created by Łukasz Lesiak on 14/11/2022.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    
+    std::cout << "Siema";
+    
 }
