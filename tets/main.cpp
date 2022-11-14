d27b05741f007e24937de15082320132595dc2d9
eddiieee <kontakt.lesiak@gmail.com>
2022-11-14 21:50:34 +0100

    Initial Commit

---
+++ tets/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tets
+//
+//  Created by Łukasz Lesiak on 14/11/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
